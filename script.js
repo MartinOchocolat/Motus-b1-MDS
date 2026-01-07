bb9ace505cc3cbad69530a0e25f22345398ac03f
Martin POUTRAIN <poutrain.martin@icloud.com>
2026-01-07 13:51:04 +0100

    initialisation du projet

---
+++ script.js
@@ -0,0 +1,31 @@
+const words = [];
+
+function initialisation(){
+    /*Fonction pour vider la grille et réinitialiser le nombre d'essais*/
+    
+}
+
+function wordSize(word){
+    /*Fonction qui vérifie que le mot rentré fait bien 5 lettres*/
+
+}
+
+function answerVerif(tab){
+    /*Fonction qui vérifie si le mot rentré est bon, et quelles lettres sont bonnes*/
+
+}
+
+function color(){
+    /*Fonction qui va gérer l'affichage graphique en changeant les couleurs des cases*/
+
+}
+
+function endWrong(){
+    /*Fonction qui affiche le mot entier en cas de défaite*/
+}
+
+function motus(){
+    /* Fonction principale du programme*/
+
+}
+
